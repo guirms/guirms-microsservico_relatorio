c2b8d77c8d7c99804d4d8199b3011d0221968dae
guirms <guilhermesantana84@hotmail.com>
2023-02-21 13:07:13 -0300

    feat: implementando controller para testes

---
+++ Web/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("Teste")]
+    public class TesteController
+    {
+        [HttpPost("TesteConsole")]
+        public void TesteConsole(string texto)
+        {
+            Console.WriteLine(texto);
+        }
+    }
+}
+
